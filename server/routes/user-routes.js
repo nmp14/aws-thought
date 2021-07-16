2691485d4f0398fd4565243118278974b594df4d
Nathan Peek <nmp14fsu@gmail.com>
2021-07-16 04:00:43 -0400

    Create front-end requests and host dynamondb

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -4,10 +4,9 @@ const router = express.Router();
 const AWS = require("aws-sdk");
 const awsConfig = {
     region: "us-east-2",
-    endpoint: "http://localhost:8000",
-
 };
 AWS.config.update(awsConfig);
+
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = "thoughts";
 
